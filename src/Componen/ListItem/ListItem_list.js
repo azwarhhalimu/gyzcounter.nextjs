f995c8f8d16f6f3dc32e023218862de0c188f83b
azwarhhalimu <azwarhalimu@gmail.com>
2023-04-06 23:49:30 +0800

    fixed

--- src/Componen/ListItem/ListItem_list.js
+++ src/Componen/ListItem/ListItem_list.js
@@ -1,34 +1,38 @@
 import { baseUrl, urlEncode } from "@/Utils/Config";
+import Image from "next/image";
 
 
 const ListItem_list = ({ list }) => {
 
     return <>
         <table width={"100%"}>
-            {list.map((data, index) => (
-                <>
-                    <tr id="link" style={{ cursor: "pointer" }} onClick={() => {
-                        //  navigasi("/produk/" + data["id_produk"] + "/" + urlEncode(data["nama_produk"]) + ".html");
-                    }}>
-                        <td>
-                            <img style={{ borderRadius: "5px" }} src={baseUrl("images/produk?w=50&s=" + data["foto"])} />
-                        </td>
-                        <td width={"10px"}>
+            <thead>
+                {list.map((data, index) => (
+                    <>
+                        <tr id="link" style={{ cursor: "pointer" }} onClick={() => {
+                            //  navigasi("/produk/" + data["id_produk"] + "/" + urlEncode(data["nama_produk"]) + ".html");
+                        }}>
+                            <td>
+                                <Image style={{ borderRadius: "5px" }} src={baseUrl("images/produk?w=50&s=" + data["foto"])} />
+                            </td>
+                            <td width={"10px"}>
 
-                        </td>
-                        <td>
-                            <div style={{ fontWeight: "400", }}>{data["nama_produk"]}</div>
-                            <div style={{ fontSize: "12px" }}>Rp. {data["harga"]}</div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colSpan={3}>
-                            <hr />
-                        </td>
-                    </tr>
-                </>
+                            </td>
+                            <td>
+                                <div style={{ fontWeight: "400", }}>{data["nama_produk"]}</div>
+                                <div style={{ fontSize: "12px" }}>Rp. {data["harga"]}</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={3}>
+                                <hr />
+                            </td>
+                        </tr>
+                    </>
 
-            ))}
+                ))}
+            </thead>
+
 
         </table>
     </>
