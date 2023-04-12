a50ec7ba52bcd6859b94eac6cfe8103eb9abc6ce
azwarhhalimu <azwarhalimu@gmail.com>
2023-04-12 22:24:20 +0800

    loadng

--- src/pages/kategori/[...kategori].js
+++ src/pages/kategori/[...kategori].js
@@ -6,6 +6,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import qs from "query-string"
 import Loading_save from "@/Utils/Loading_save";
+import No_data from "@/Widget/No_data";
 
 const Kategori = () => {
     const [aktif, setAktif] = useState("");
@@ -14,6 +15,7 @@ const Kategori = () => {
     const [data, setData] = useState([]);
     const [id, setId] = useState("");
     const [loading, setLoading] = useState(false);
+    const [load, setLoad] = useState(false);
 
     const url = typeof window !== "undefined" && window.location.pathname;
     useEffect(() => {
@@ -31,12 +33,14 @@ const Kategori = () => {
 
     const [kategori, setkategori] = useState([]);
     const _getData = (id) => {
+        setLoad(true)
         axios.post(baseUrl("public/list_produk_kategori"),
             qs.stringify({
                 "id_kategori": id
             })
         )
             .then(respon => {
+                setLoad(false);
                 setName(respon.data.kategori);
                 setData(respon.data.data);
             })
@@ -74,37 +78,44 @@ const Kategori = () => {
                     </div>
                 </div>
                 <div className="col-lg-8">
+                    <h3>{name}</h3>
                     <div className="row">
-                        {data.map((list, index) => (
-                            <div key={index + 'fdjkf'} className="col-sm-3 " {...(index > 3) && {
-                                style: {
-                                    marginTop: "20px"
-                                }
-                            }}>
-                                <Link href={"/produk/" + list["id_produk"] + "/" + Url_encoded(list["nama_produk"]) + ".html"} style={{ color: "#333333" }}>
-                                    <div className="list-data" style={{
-                                        padding: "2px",
-                                        border: "1px solid #dfdfdf",
-                                        borderRadius: "10px"
+                        {load ? <div style={{ textAlign: "center", width: "100%" }}>
+                            <Height height={100} />
+                            <Loading_save text={"Mengambil data kategori..."} />
+                        </div> : <>
+                            {data.map((list, index) => (
+                                <div key={index + 'fdjkf'} className="col-sm-3 " {...(index > 3) && {
+                                    style: {
+                                        marginTop: "20px"
+                                    }
+                                }}>
+                                    <Link href={"/produk/" + list["id_produk"] + "/" + Url_encoded(list["nama_produk"]) + ".html"} style={{ color: "#333333" }}>
+                                        <div className="list-data" style={{
+                                            padding: "2px",
+                                            border: "1px solid #dfdfdf",
+                                            borderRadius: "10px"
 
-                                    }}>
-                                        <img style={{ width: "100%", borderRadius: "10px" }} src={baseUrl("images/produk?w=250&s=" + list["foto"])} />
-                                        <Height height={5} />
-                                        <div style={{ padding: "5px" }}>
-                                            <div style={{ lineHeight: "100%", fontSize: "14px", fontWeight: "500", height: "40px" }}>
-                                                {list["nama_produk"]}
-                                            </div>
-                                            <div style={{ fontSize: "13px" }}>
-                                                Rp. {list["harga"]}
+                                        }}>
+                                            <img style={{ width: "100%", borderRadius: "10px" }} src={baseUrl("images/produk?w=250&s=" + list["foto"])} />
+                                            <Height height={5} />
+                                            <div style={{ padding: "5px" }}>
+                                                <div style={{ lineHeight: "100%", fontSize: "14px", fontWeight: "500", height: "40px" }}>
+                                                    {list["nama_produk"]}
+                                                </div>
+                                                <div style={{ fontSize: "13px" }}>
+                                                    Rp. {list["harga"]}
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </Link>
+                                    </Link>
 
-                            </div >
-                        ))}
+                                </div >
+                            ))}
+                        </>}
                     </div>
 
+                    {data.length == 0 && <No_data title={"Opsss"} text={"Data kategori ini masih kosong"} />}
                 </div>
             </div>
         </div >
